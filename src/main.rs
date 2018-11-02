0e49a3131352e9afe7a993fce5d0fa3fa73c3874
VitalyR <vitalyankh@gmail.com>
2018-11-02 22:05:30 +0800

    Add function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate clap;
 extern crate indicatif;
+extern crate reqwest;
 
-use indicatif::ProgressBar;
+
+use reqwest::*;
 use clap::{App, Arg};
+use indicatif::{ProgressBar, ProgressStyle};
 
 fn main() {
     let matches = App::new("Rget")
@@ -19,13 +22,40 @@ fn main() {
     println!("{}", url);
 }
 
-fn create_progress_bar(quiet_mode:bool,msg:&str,length:Option<u64>)->ProgressBar{
-    let bar=match quiet_mode{
-        true=>ProgressBar::hidden(),
-        false=>match length{
-            Some(len)=>ProgresssBar::new(len),
-            None=>ProgressBar::new_spinner,
+fn create_progress_bar(quiet_mode: bool, msg: &str, length: Option<u64>) -> ProgressBar {
+    let bar = match quiet_mode {
+        true => ProgressBar::hidden(),
+        false => match length {
+            Some(len) => ProgressBar::new(len),
+            None => ProgressBar::new_spinner(),
         }
     };
+    bar.set_message(msg);
+    match length.is_some() {
+        true => bar
+            .set_style(ProgressStyle::default_bar()
+                .template("{msg} {spinner:.green} [{elapsed_precise}]\
+                [{wide_bar}:.cyan/blue}] {bytes}/{total_bytes} eta: {eta}")
+                .progress_chars("=> ")),
+        false => bar.set_style(ProgressStyle::default_spinner()),
+    };
+    bar
+}
 
+
+fn download(target:&str,quiet_mode:bool)->Result<(),Box<std::error::Error>>{
+    // parse url
+    let url=parse_url(target)?;
+    let client=Client::new().unwrap();
+    let mut resp=client.get_url()?
+        .send()
+        .unwrap();
+//    print(format!("HTTP request sent... {}",
+//    style(format!("{}",resp.status())).green()),
+//    quiet_mode);
+    if resp.status().is_success(){
+        let header=resp.header().clone();
+    }
+
+    Ok(())
 }
